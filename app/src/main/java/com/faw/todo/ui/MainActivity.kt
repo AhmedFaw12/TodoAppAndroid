e325107b87d6fc0577d18e97651bf6d5b2889d12
Ahmed Fawzy <ahmedfawdw12@gmail.com>
2023-02-08 15:35:51 +0200

    3- Added Splash Screen

---
+++ app/src/main/java/com/faw/todo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.faw.todo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.faw.todo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
